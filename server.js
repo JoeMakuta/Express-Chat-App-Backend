ca6a85bde9980fd60ed7611428900e0d027403c1
Josué Makuta <makutajosue@gmail.com>
2022-11-08 13:51:37 +0100

    Feat : DB Connexion

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 const PORT = 5000 || process.env.PORT
+const dbUrl = process.env.DBURI
+
+//DB Connection
+mongoose.connect(dbUrl)
+   .then(() => console.log("DB Connexion succeded"))
+   .catch((err) => console.log("An error occured", err))
+
 
 const app = express();
 app
    .use('/', (req, res) => {
-      res.send('Hello world ...')
+      res.json({ message: "Hello world ..." })
    })
    .listen(PORT)
