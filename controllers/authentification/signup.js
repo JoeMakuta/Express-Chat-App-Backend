7d4a0d967d78bd8b6c4461a28b1f11608bfe00ee
Josué Makuta <makutajosue@gmail.com>
2022-11-10 15:32:06 +0100

    Feat :  checking if email has been used before signup

--- controllers/authentification/signup.js
+++ controllers/authentification/signup.js
@@ -3,30 +3,42 @@ const cryptPassword = require('./cryptPassword')
 
 const signUp = async (req, res) => {
    const { userName, userEmail, passWord } = req.body
+
    if (!userName || !userEmail || !passWord) {
       res.status(400).json({ message: "Fill the fields ..." })
-   } else {
+   }
+   else {
       console.log('The password is ', passWord);
       const cryptedPassword = await cryptPassword(passWord);
       console.log('The crypted password is ', cryptedPassword);
-      const user = new UserModel({
-         "userName": userName,
-         "userEmail": userEmail,
-         "passWord": cryptedPassword,
-      })
-      user.save()
-         .then((data) => {
-            res.status(200).json({
-               message: "Data saved success",
-               data: data
-            })
+
+      UserModel.findOne({ userEmail: req.body.userEmail })
+         .then(user => {
+            if (user) {
+               res.status(403).json({ status: 403, message: 'Email used' })
+            } else {
+               const user = new UserModel({
+                  "userName": userName,
+                  "userEmail": userEmail,
+                  "passWord": cryptedPassword,
+               })
+               user.save()
+                  .then((data) => {
+                     res.status(200).json({
+                        message: "Data saved success",
+                        data: data
+                     })
+                  })
+                  .catch((err) => {
+                     res.status(400).json({
+                        message: "Did not save data",
+                        error: err
+                     })
+                  })
+            }
          })
-         .catch((err) => {
-            res.json({
-               message: "Did not save data",
-               error: err
-            })
-         })
+
+
 
    }
 
